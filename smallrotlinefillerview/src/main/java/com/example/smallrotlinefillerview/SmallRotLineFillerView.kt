771b3dc980ab3c5c3b03be1ee4ee9d2fcedb683e
Anwesh43 <anweshthecool0@gmail.com>
2020-12-18 03:59:08 +0530

    adding SLRFNode class

--- smallrotlinefillerview/src/main/java/com/example/smallrotlinefillerview/SmallRotLineFillerView.kt
+++ smallrotlinefillerview/src/main/java/com/example/smallrotlinefillerview/SmallRotLineFillerView.kt
@@ -125,4 +125,45 @@ class SmallRotLineFillerView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SRLFNode(var i : Int, val state : State = State()) {
+
+        private var next : SRLFNode? = null
+        private var prev : SRLFNode? = null
+
+        init {
+
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = SRLFNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSRLFNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SRLFNode {
+            var curr : SRLFNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
