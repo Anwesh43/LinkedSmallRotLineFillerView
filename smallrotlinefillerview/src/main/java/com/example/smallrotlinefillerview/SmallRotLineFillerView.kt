e7faed11e846ecdd392f4bcf95886533329e6710
Anwesh43 <anweshthecool0@gmail.com>
2020-12-18 03:43:27 +0530

    adding Canvas extension methods

--- smallrotlinefillerview/src/main/java/com/example/smallrotlinefillerview/SmallRotLineFillerView.kt
+++ smallrotlinefillerview/src/main/java/com/example/smallrotlinefillerview/SmallRotLineFillerView.kt
@@ -25,8 +25,40 @@ val strokeFactor : Float = 90f
 val sizeFactor : Float = 3.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawSmallRotLine(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val gap : Float = (2 * size) / lines
+    save()
+    translate(w / 2 - size, h / 2)
+    for (j in 0..(lines - 1)) {
+        save()
+        translate(gap * j, 0f)
+        rotate(-deg * sf2.divideScale(j, parts))
+        drawLine(0f, 0f, gap * sf1.divideScale(j, parts), 0f, paint)
+        if (j != (lines - 1)) {
+            drawRect(RectF(0f, -gap * sf3.divideScale(j, parts), gap, 0f), paint)
+        }
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawSRLFNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawSmallRotLine(scale, w, h, paint)
+}
