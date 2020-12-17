bbf95606cf6f03d91f98ebbf7da6f448806d42f2
Anwesh43 <anweshthecool0@gmail.com>
2020-12-18 04:16:15 +0530

    adding fullscreen support

--- app/src/main/java/com/example/linkedsmallrotlinefillerview/MainActivity.kt
+++ app/src/main/java/com/example/linkedsmallrotlinefillerview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedsmallrotlinefillerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.smallrotlinefillerview.SmallRotLineFillerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SmallRotLineFillerView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
